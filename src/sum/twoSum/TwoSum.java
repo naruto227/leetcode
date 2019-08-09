efcbd65f52ddc32fce0e763e3c06e161e15d916e
Michael Allan <huangzq30@163.com>
2019-08-10 01:47:52 +0800

    commit two sum

---
+++ src/sum/twoSum/TwoSum.java
@@ -0,0 +1,75 @@
+package sum.twoSum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Michael Allan on 2019/8/10.
+ */
+public class TwoSum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] nums = stringToIntegerArray(line);
+            line = in.readLine();
+            int target = Integer.parseInt(line);
+            int[] ret = new Solution().twoSum(nums, target);
+
+            String out = integerArrayToString(ret);
+            System.out.println(out);
+        }
+    }
+
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if(input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for (int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+
+        return output;
+    }
+
+    public static String integerArrayToString(int[] nums) {
+        return integerArrayToString(nums, nums.length);
+    }
+
+    public static String integerArrayToString(int[] nums, int length) {
+        if (length == 0) {
+            return "[]";
+        }
+
+        String result = "";
+        for (int index = 0; index < length; index++) {
+            int number = nums[index];
+            result += Integer.toString(number) + ", ";
+        }
+
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if (map.containsKey(temp)) {
+                return new int[] {map.get(temp), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {-1, -1};
+    }
+}
