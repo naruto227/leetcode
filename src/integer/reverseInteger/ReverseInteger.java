fba1d8b8f8cdf2be30ea8bddbff8e0c10fbe9540
Michael Allan <huangzq30@163.com>
2019-08-22 00:32:38 +0800

    Q7 Reverse Integer

--- src/integer/reverseInteger/ReverseInteger.java
+++ src/integer/reverseInteger/ReverseInteger.java
@@ -6,23 +6,13 @@ import java.io.InputStreamReader;
 
 /**
  * Given a 32-bit signed integer, reverse digits of an integer.
+ * Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 
- Example 1:
-
- Input: 123
- Output: 321
- Example 2:
-
- Input: -123
- Output: -321
- Example 3:
-
- Input: 120
- Output: 21
  * Created by Michael Allan on 2019/8/21.
  */
 public class ReverseInteger {
     public static void main(String[] args) throws IOException {
+        System.out.println(Integer.MAX_VALUE + "\n" + Integer.MIN_VALUE);
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String line;
         while ((line = in.readLine()) != null) {
@@ -38,6 +28,37 @@ public class ReverseInteger {
 
 class Solution {
     public int reverse(int x) {
-        return 0;
+        int sign = 1;
+        if (x < 0) {
+            sign = -1;
+            x = Math.abs(x);
+        }
+        long sum = 0;
+        while (x != 0) {
+            sum = sum * 10 + x % 10;
+            x /= 10;
+        }
+        sum *= sign;
+        if (sum > Integer.MAX_VALUE || sum < Integer.MIN_VALUE) {
+            return 0;
+        }else {
+            return (int) sum;
+        }
+    }
+
+    public int reverse2(int x) {
+        int sum = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (sum > Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+            if (sum < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+            sum = sum * 10 + pop;
+        }
+        return sum;
     }
 }
