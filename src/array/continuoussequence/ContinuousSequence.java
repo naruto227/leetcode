863241c6f0d8f3df275c9db4bbfc6f4d91e7a4bf
Michael Allan <huangzq30@163.com>
2020-03-07 23:06:42 +0800

    每日一题 和为s的连续正数序列 增加双指针方法

--- src/array/continuoussequence/ContinuousSequence.java
+++ src/array/continuoussequence/ContinuousSequence.java
@@ -20,12 +20,17 @@ public class ContinuousSequence {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line;
         while ((line = br.readLine()) != null) {
-            int[][] res = new Solution().findContinuousSequence1(Integer.valueOf(line));
+            int[][] res = new Solution().findContinuousSequence2(Integer.valueOf(line));
             String deepToString = Arrays.deepToString(res);
             System.out.println(deepToString);
         }
     }
     private static class Solution {
+        /**
+         * 暴力：遍历
+         * @param target
+         * @return
+         */
         public int[][] findContinuousSequence(int target) {
             int temp = (target + 1) >> 1;
             Deque<int[]> deque = new ArrayDeque<>();
@@ -60,6 +65,11 @@ public class ContinuousSequence {
             return res;
         }
 
+        /**
+         * 滑动窗口法
+         * @param target
+         * @return
+         */
         public int[][] findContinuousSequence1(int target) {
             List<int[]> list = new ArrayList<>();
             int sum = 1;
@@ -86,6 +96,62 @@ public class ContinuousSequence {
             }
             return list.toArray(new int[list.size()][]);
         }
+
+        /**
+         * 双指针法
+         * @param target
+         * @return
+         */
+        public int[][] findContinuousSequence2(int target) {
+            List<int[]> list = new ArrayList<>();
+            int low = 1;
+            int high = 2;
+            while (low < high) {
+                int sum = (low + high) * (high - low + 1) / 2;
+                if(sum < target) {
+                    high++;
+                }else if (sum > target) {
+                    low++;
+                }else {
+                    int[] arr = new int[high - low + 1];
+                    for (int i = 0; i < arr.length; i++) {
+                        arr[i] = i + low;
+                    }
+                    list.add(arr);
+                    sum -= low;
+                    low++;
+                }
+            }
+            return list.toArray(new int[list.size()][]);
+        }
+
+        /**
+         * 听说很快，记录一下
+         * @param target
+         * @return
+         */
+        public int[][] findContinuousSequence3(int target) {
+
+            List<int[]> result = new ArrayList<>();
+            int i = 1;
+
+            while(target>0)
+            {
+                target -= i++;
+                if(target>0 && target%i == 0)
+                {
+                    int[] array = new int[i];
+                    for(int k = target/i, j = 0; k < target/i+i; k++,j++)
+                    {
+                        array[j] = k;
+                    }
+                    result.add(array);
+                }
+            }
+            Collections.reverse(result);
+            return result.toArray(new int[0][]);
+        }
+
     }
 
 }
