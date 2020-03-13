a19537530604fbe6d38bb95646358f3a166c3632
Michael Allan <huangzq30@163.com>
2020-03-13 21:53:11 +0800

    每日一题 求解中位数 摩尔投票法

--- src/array/majorityelement/MajorityElement.java
+++ src/array/majorityelement/MajorityElement.java
@@ -9,6 +9,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 169. Majority Element
+ * Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+ You may assume that the array is non-empty and the majority element always exist in the array.
+
+ 链接：https://leetcode-cn.com/problems/majority-element
  * Created by Michael Allan on 2020/3/13.
  */
 public class MajorityElement {
