7a59e09a5bae68a75137d15c2f7db7dc2f180f76
Michael Allan <huangzq30@163.com>
2020-04-07 23:48:30 +0800

    每日一题 移除链表元素 虚拟头节点

--- src/LinkedList/deletenode/DeleteNode.java
+++ src/LinkedList/deletenode/DeleteNode.java
@@ -28,4 +28,22 @@ public class DeleteNode {
 
         return head;
     }
+
+    public ListNode removeElements1(ListNode head, int val) {
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode pre = dummyHead;
+
+        while (pre.next != null) {
+            if (pre.next.val == val) {
+                ListNode delNode = pre.next;
+                pre.next = pre.next.next;
+                delNode.next = null;
+            }else {
+                pre = pre.next;
+            }
+        }
+
+        return dummyHead.next;
+    }
 }
