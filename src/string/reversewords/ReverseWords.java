2fd9f2c8a722daf0f21d49e5ad9a23a6d2c0dd3c
Michael Allan <huangzq30@163.com>
2020-04-11 00:16:51 +0800

    每日一题 翻转字符串里的单词

--- src/string/reversewords/ReverseWords.java
+++ src/string/reversewords/ReverseWords.java
@@ -3,6 +3,8 @@ package string.reversewords;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 /**
  * 链接：https://leetcode-cn.com/problems/reverse-words-in-a-string/
@@ -15,11 +17,17 @@ public class ReverseWords {
         String line;
         Solution solution = new Solution();
         while ((line = br.readLine()) != null) {
-
+            String reverseWords = solution.reverseWords2(line);
+            System.out.println(reverseWords);
         }
     }
 
     static class Solution {
+        /**
+         * 这个方法运行速度最快，why?
+         * @param s
+         * @return
+         */
         public String reverseWords(String s) {
             if (s == null || s.trim().length() == 0) {
                 return "";
@@ -53,6 +61,34 @@ public class ReverseWords {
 //
 //            return String.join(" ", wordList);
 
+        }
+
+        public String reverseWords2(String s) {
+            int left = 0;
+            int right = s.length() - 1;
+            char[] chars = s.toCharArray();
+            while (left <= right && chars[left] == ' ') {
+                left++;
+            }
+            while (left <= right && chars[right] == ' ') {
+                right--;
+            }
+
+            StringBuilder word = new StringBuilder();
+            Deque<String> res = new ArrayDeque<>();
+
+            while (left <= right) {
+                if (chars[left] == ' ' && word.length() > 0) {
+                    res.addFirst(word.toString());
+                    word.setLength(0);
+                } else if (chars[left] != ' ') {
+                    word.append(chars[left]);
+                }
+                left++;
+            }
+            res.addFirst(word.toString());
+
+            return String.join(" ", res);
         }
 
     }
