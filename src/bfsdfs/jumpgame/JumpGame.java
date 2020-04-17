f6da11e91c915fa069c5c733df9fbca8227ee077
Michael Allan <huangzq30@163.com>
2020-04-17 23:11:30 +0800

    每日一题 跳跃游戏 贪心思路

--- src/bfsdfs/jumpgame/JumpGame.java
+++ src/bfsdfs/jumpgame/JumpGame.java
@@ -34,10 +34,9 @@ public class JumpGame {
             queue.offer(new int[] { 0, nums[0] });
             boolean[] visited = new boolean[nums.length];
             visited[0] = true;
-            int lastIndex = 0;
             while (!queue.isEmpty()) {
                 int[] node = queue.poll();
-                for (int i = 1; i <= node[1]; i++) {
+                for (int i = 1; i < nums.length && i <= node[1]; i++) {
                     int index = node[0] + i;
                     // 当走到了新的索引的位置，刚好是最后一个元素时，返回true
                     if (index == end) {
@@ -49,8 +48,27 @@ public class JumpGame {
                     if (!visited[index]) {
                         queue.offer(newNode);
                         visited[index] = true;
-                        lastIndex = index;
                     }
+                }
+            }
+            return false;
+        }
+
+        /**
+         * 贪心算法，遍历，找到最远可到达的位置
+         * @param nums
+         * @return
+         */
+        public boolean canJump1(int[] nums) {
+            int len = nums.length;
+            int remote = 0;
+            for (int i = 0; i < len; i++) {
+                // 第i个位置可达
+                if (remote >= i) {
+                    if (remote >= len - 1) {
+                        return true;
+                    }
+                    remote = Math.max(remote, i + nums[i]);
                 }
             }
             return false;
