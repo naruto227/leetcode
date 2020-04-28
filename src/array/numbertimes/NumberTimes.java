3005a54962721d3e0f5214a34bc04c7fe6319ccb
Michael Allan <huangzq30@163.com>
2020-04-29 00:02:19 +0800

    每日一题 数组中数字出现的次数 使用位运算 异或

--- src/array/numbertimes/NumberTimes.java
+++ src/array/numbertimes/NumberTimes.java
@@ -48,5 +48,36 @@ public class NumberTimes {
             }
             return res;
         }
+
+        /**
+         * 思路：简化版，一个数组中只有一个数字出现一次，其它数字出现两次，如何找这个出现一次的数字？
+         * 所有数字做异或，相同为0，相异为1，所以最后剩下就是要找的数字。
+         * 现在的问题是由两个出现一次的数字，可以知道全部做异或肯定不为0，假设为x。按照上面简化版的思路，
+         * 如果能分两组，相同的数字出现在一组中，要找的数字出现在不同的组中，然后两组数组分别全部异或，就可以
+         * 找到答案了。x != 0，所以x的二进制位中至少有一个位x(i)是1。
+         * 所以可以这样分组：数字二进制位的第x(i)位是0的分一组，是1的分另一组。
+         * @param nums
+         * @return
+         */
+        public int[] singleNumbers1(int[] nums) {
+            int temp = 0;
+            for (int num : nums) {
+                temp ^= num;
+            }
+            int div = 1;
+            while ((div & temp) == 0) {
+                div <<= 1;
+            }
+            int a = 0;
+            int b = 0;
+            for (int num : nums) {
+                if((div & num) == 0) {
+                    a ^= num;
+                }else {
+                    b ^= num;
+                }
+            }
+            return new int[] {a, b};
+        }
     }
 }
