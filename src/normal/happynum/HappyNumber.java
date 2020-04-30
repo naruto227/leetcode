e2371b3ce5b794c3b6700fa5e56ff8450e8901d5
Michael Allan <huangzq30@163.com>
2020-04-30 20:28:04 +0800

    每日一题 快乐数 弗洛伊德循环查找算法

--- src/normal/happynum/HappyNumber.java
+++ src/normal/happynum/HappyNumber.java
@@ -17,12 +17,17 @@ public class HappyNumber {
             Integer num = Integer.valueOf(line);
             double pow = Math.pow(num, 2);
             System.out.println(pow);
-            boolean happy = solution.isHappy(num);
+            boolean happy = solution.isHappy1(num);
             System.out.println(happy);
         }
     }
 
     class Solution {
+        /**
+         * 计算n的下一个数字，用一个集合保存已经记录过的值
+         * @param n
+         * @return
+         */
         public boolean isHappy(int n) {
             if (n <= 0) {
                 return false;
@@ -31,7 +36,7 @@ public class HappyNumber {
             linkedList.offer(n);
             for (;;) {
                 int num = linkedList.getLast();
-//                System.out.print(num + "\t");
+                System.out.print(num + "\t");
                 if (num == 1) {
                     return true;
                 }
@@ -45,6 +50,32 @@ public class HappyNumber {
                 }
                 linkedList.offer(res);
             }
+        }
+
+        /**
+         * 弗洛伊德循环查找算法，快跑者、慢跑者，在一个循环内，快跑者和慢跑者必然会相遇
+         * @param n
+         * @return
+         */
+        public boolean isHappy1(int n) {
+            int slowRunner = n;
+            int fastRunner = getNext(n);
+            while (fastRunner != 1 && slowRunner != fastRunner) {
+                slowRunner = getNext(slowRunner);
+                fastRunner = getNext(getNext(fastRunner));
+            }
+
+            return fastRunner == 1;
+        }
+
+        private int getNext(int n) {
+            int totalSum = 0;
+            while (n != 0) {
+                int temp = n % 10;
+                totalSum += temp * temp;
+                n /= 10;
+            }
+            return totalSum;
         }
     }
 }
