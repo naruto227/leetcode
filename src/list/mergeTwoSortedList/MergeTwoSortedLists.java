309abfb150fe35b5d3344a399ae10c1e562a1c0d
Michael Allan <huangzq30@163.com>
2020-05-01 08:41:30 +0800

    每日一题 合并两个有序链表

--- src/list/mergeTwoSortedList/MergeTwoSortedLists.java
+++ src/list/mergeTwoSortedList/MergeTwoSortedLists.java
@@ -20,32 +20,50 @@ public class MergeTwoSortedLists {
             ListNode l1 = ListNodeUtil.stringToListNode(line);
             line = br.readLine();
             ListNode l2 = ListNodeUtil.stringToListNode(line);
-            ListNode listNode = new Solution().mergeTwoLists(l1, l2);
+            ListNode listNode = new MergeTwoSortedLists().new Solution().mergeTwoLists(l1, l2);
             String listNodeToString = ListNodeUtil.listNodeToString(listNode);
             System.out.println(listNodeToString);
         }
     }
-}
 
-class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode p = new ListNode(0);
-        ListNode head = p;
-        while(!(l1 == null || l2 == null)) {
-            if(l1.val < l2.val) {
-                p.next = l1;
-                l1 = l1.next;
-            }else {
-                p.next = l2;
-                l2 = l2.next;
+    class Solution {
+        public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+            ListNode p = new ListNode(0);
+            ListNode head = p;
+            while(!(l1 == null || l2 == null)) {
+                if(l1.val < l2.val) {
+                    p.next = l1;
+                    l1 = l1.next;
+                }else {
+                    p.next = l2;
+                    l2 = l2.next;
+                }
+                p = p.next;
             }
-            p = p.next;
+            if(l1 == null) {
+                p.next = l2;
+            }else if(l2 == null) {
+                p.next = l1;
+            }
+            return head.next;
         }
-        if(l1 == null) {
-            p.next = l2;
-        }else if(l2 == null) {
-            p.next = l1;
+
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            if (l1 == null) {
+                return l2;
+            }
+            if (l2 == null) {
+                return l1;
+            }
+            if (l1.val < l2.val) {
+                l1.next = mergeTwoLists(l1.next, l2);
+                return l1;
+            } else {
+                l2.next = mergeTwoLists(l1, l2.next);
+                return l2;
+            }
         }
-        return head.next;
     }
 }
+
+
