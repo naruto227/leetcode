3184e6e001a552fde8ee98a967eece2f00d627ac
Michael Allan <huangzq30@163.com>
2020-05-01 09:33:47 +0800

    每日一题 合并两个有序链表 简化的迭代代码

--- src/list/mergeTwoSortedList/MergeTwoSortedLists.java
+++ src/list/mergeTwoSortedList/MergeTwoSortedLists.java
@@ -48,20 +48,37 @@ public class MergeTwoSortedLists {
             return head.next;
         }
 
+//        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+//            if (l1 == null) {
+//                return l2;
+//            }
+//            if (l2 == null) {
+//                return l1;
+//            }
+//            if (l1.val < l2.val) {
+//                l1.next = mergeTwoLists(l1.next, l2);
+//                return l1;
+//            } else {
+//                l2.next = mergeTwoLists(l1, l2.next);
+//                return l2;
+//            }
+//        }
+
         public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-            if (l1 == null) {
-                return l2;
+            ListNode dummyHead = new ListNode(-1);
+            ListNode head = dummyHead;
+            while (!(l1 == null || l2 == null)) {
+                if (l1.val < l2.val) {
+                    head.next = l1;
+                    l1 = l1.next;
+                } else {
+                    head.next = l2;
+                    l2 = l2.next;
+                }
+                head = head.next;
             }
-            if (l2 == null) {
-                return l1;
-            }
-            if (l1.val < l2.val) {
-                l1.next = mergeTwoLists(l1.next, l2);
-                return l1;
-            } else {
-                l2.next = mergeTwoLists(l1, l2.next);
-                return l2;
-            }
+            head.next = l1 == null ? l2 : l1;
+            return dummyHead.next;
         }
     }
 }
