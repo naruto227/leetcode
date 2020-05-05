05c0c4e071c17e0a3364359e274402f1c0794954
Michael Allan <huangzq30@163.com>
2020-05-06 00:24:34 +0800

    每日一题 有效的二分搜索树 增加中序遍历的写法

--- src/tree/validatebst/ValidateBST.java
+++ src/tree/validatebst/ValidateBST.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
+ * BST：左子树的所有节点都小于根节点，右子树的所有节点都大于根节点
  * 98. Validate Binary Search Tree
  * 链接：https://leetcode-cn.com/problems/validate-binary-search-tree/
  * Created by Michael Allan on 2020/5/5.
@@ -20,12 +21,17 @@ public class ValidateBST {
         String line;
         while ((line = br.readLine()) != null) {
             TreeNode treeNode = TreeUtil.createTree(ArrayUtil.stringToIntegerArray(line));
-            boolean validBST = solution.isValidBST(treeNode);
+            boolean validBST = solution.isValidBST1(treeNode);
             System.out.println(validBST);
         }
     }
 
     class Solution {
+        /**
+         * 递归的实现
+         * @param root
+         * @return
+         */
         public boolean isValidBST(TreeNode root) {
             return helper(root, null, null);
         }
@@ -42,6 +48,30 @@ public class ValidateBST {
                 return false;
             }
             return helper(root.left, lower, val) && helper(root.right, val, upper);
+        }
+
+        /**
+         * 使用中序遍历的解法
+         * @param root
+         * @return
+         */
+        private long prev = Long.MIN_VALUE;
+        public boolean isValidBST1(TreeNode root) {
+            return inOrder(root);
+        }
+
+        private boolean inOrder(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+            if (!inOrder(root.left)) {
+                return false;
+            }
+            if (root.val <= prev) {
+                return false;
+            }
+            prev = root.val;
+            return inOrder(root.right);
         }
     }
 }
