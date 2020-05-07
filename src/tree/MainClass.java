6b39760f9aa5ab04e755cd2201d5c903c84fa5a2
Michael Allan <huangzq30@163.com>
2020-05-07 23:34:19 +0800

    每日一题 一棵树是另一棵树的子树

---
+++ src/tree/MainClass.java
@@ -0,0 +1,120 @@
+package tree;
+
+/**
+ * Created by Michael Allan on 2020/5/7.
+ */
+/* -----------------------------------
+ *  WARNING:
+ * -----------------------------------
+ *  Your code may fail to compile
+ *  because it contains public class
+ *  declarations.
+ *  To fix this, please remove the
+ *  "public" keyword from your class
+ *  declarations.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (t == null) return true;   // t 为 null 一定都是 true
+        if (s == null) return false;  // 这里 t 一定不为 null, 只要 s 为 null，肯定是 false
+        return isSubtree(s.left, t) || isSubtree(s.right, t) || isSameTree(s,t);
+    }
+
+    /**
+     * 判断两棵树是否相同
+     */
+    public boolean isSameTree(TreeNode s, TreeNode t){
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
+        if (s.val != t.val) return false;
+        return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+    }
+}
+
+public class MainClass {
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+
+    public static String booleanToString(boolean input) {
+        return input ? "True" : "False";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            TreeNode s = stringToTreeNode(line);
+            line = in.readLine();
+            TreeNode t = stringToTreeNode(line);
+
+            boolean ret = new Solution().isSubtree(s, t);
+
+            String out = booleanToString(ret);
+
+            System.out.print(out);
+        }
+    }
+}
