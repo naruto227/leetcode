49abd750882c6fb33bc14e160c4801717bc84138
Michael Allan <huangzq30@163.com>
2020-06-27 23:39:03 +0800

    每日一题 缺失的第一个正数

---
+++ src/hash/firstmissingpositive/FirstMissingPositive.java
@@ -0,0 +1,32 @@
+package hash.firstmissingpositive;
+
+/**
+ * Created by Michael Allan on 2020/6/27.
+ */
+public class FirstMissingPositive {
+    class Solution {
+        public int firstMissingPositive(int[] nums) {
+            int len = nums.length;
+            for(int i = 0; i < len; i++) {
+                if(nums[i] <= 0) {
+                    nums[i] = len + 1;
+                }
+            }
+
+            for(int i = 0; i < len; i++) {
+                int index = Math.abs(nums[i]);
+                if(index <= len) {
+                    nums[index - 1] = -Math.abs(nums[index - 1]);
+                }
+            }
+
+            for(int i = 0; i < len; i++) {
+                if(nums[i] > 0) {
+                    return i + 1;
+                }
+            }
+
+            return len + 1;
+        }
+    }
+}
