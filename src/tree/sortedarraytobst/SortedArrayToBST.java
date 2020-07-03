e61fc623d9befe446cb792291b31acfa273701d4
Michael Allan <huangzq30@163.com>
2020-07-03 23:49:08 +0800

    每日一题 将有序数组转换为二叉搜索树

---
+++ src/tree/sortedarraytobst/SortedArrayToBST.java
@@ -0,0 +1,24 @@
+package tree.sortedarraytobst;
+
+import tree.TreeNode;
+
+/**
+ * Created by Michael Allan on 2020/7/3.
+ */
+public class SortedArrayToBST {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode helper(int[] nums, int left, int right) {
+        if(left > right) {
+            return null;
+        }
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+
+        return root;
+    }
+}
