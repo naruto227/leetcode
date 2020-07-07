94b214931ae128113c92a20d6b026f45f703319d
Michael Allan <huangzq30@163.com>
2020-07-08 01:44:25 +0800

    每日一题 跳水板

---
+++ src/array/divingboard/DivingBoard.java
@@ -0,0 +1,28 @@
+package array.divingboard;
+
+/**
+ * Created by Michael Allan on 2020/7/8.
+ */
+public class DivingBoard {
+
+    class Solution {
+        public int[] divingBoard(int shorter, int longer, int k) {
+            if (k == 0) {
+                return new int[]{};
+            }
+            if (shorter == longer) {
+                return new int[] {shorter * k};
+            }
+            int a1 = shorter * k;
+            int an = longer * k;
+            int diff = longer - shorter;
+            int n = (an - a1) / diff + 1;
+            int[] res = new int[n];
+            for (int i = 0; i < n; i++) {
+                res[i] =  a1 + i * diff;
+            }
+
+            return res;
+        }
+    }
+}
