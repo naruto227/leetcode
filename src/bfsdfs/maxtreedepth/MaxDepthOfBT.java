65116d79880ec62ba1a9de2dc613a85a273f3a16
Michael Allan <huangzq30@163.com>
2020-07-28 23:31:02 +0800

    每日一题 二叉树的最大深度

--- src/bfsdfs/maxtreedepth/MaxDepthOfBT.java
+++ src/bfsdfs/maxtreedepth/MaxDepthOfBT.java
@@ -3,6 +3,7 @@ package bfsdfs.maxtreedepth;
 import tree.TreeNode;
 
 import java.util.LinkedList;
+import java.util.Stack;
 
 /**
  * Created by Michael Allan on 2020/7/28.
@@ -38,6 +39,35 @@ public class MaxDepthOfBT {
             }
 
             return ans;
+        }
+
+        public int maxDepth2(TreeNode root) {
+            if (root == null)
+                return 0;
+            //stack记录的是节点，而level中的元素和stack中的元素
+            //是同时入栈同时出栈，并且level记录的是节点在第几层
+            Stack<TreeNode> stack = new Stack<>();
+            Stack<Integer> level = new Stack<>();
+            stack.push(root);
+            level.push(1);
+            int max = 0;
+            while (!stack.isEmpty()) {
+                //stack中的元素和level中的元素同时出栈
+                TreeNode node = stack.pop();
+                int temp = level.pop();
+                max = Math.max(temp, max);
+                if (node.left != null) {
+                    //同时入栈
+                    stack.push(node.left);
+                    level.push(temp + 1);
+                }
+                if (node.right != null) {
+                    //同时入栈
+                    stack.push(node.right);
+                    level.push(temp + 1);
+                }
+            }
+            return max;
         }
     }
 }
